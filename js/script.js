f419e1e4245854df0d161444579228832f84de45
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-07-08 14:23:52 +0200

    Complete All!

--- js/script.js
+++ js/script.js
@@ -3,19 +3,27 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-var mailUtente = prompt('Salve, inserisca l\'email per effettuare il controllo:');
 var mails = ['francescoz@gmail.com', 'flaviorenna@hotmail.it', 'luigipira84@yahoo.it', 'federicorusso99@gmail.com', 'ferdinandoarago@gmail.com'];
-var testaccesso = false;
 
-for (i = 0; i < mails.length; i++) {
-    if (mails[i] == mailUtente) {
-        testaccesso = true;
-        break;
+function check() {
+    var mailUtente = document.getElementById('email').value;
+    var testaccesso = false;
+
+    if (mailUtente == '') {
+        alert('Non lasciare il campo vuoto');
+        return;
     }
-}
 
-if (testaccesso == true) {
-    alert('Accesso Consentito');
-} else {
-    alert('Accesso Negato');
+    for (i = 0; i < mails.length; i++) {
+        if (mails[i] == mailUtente) {
+            testaccesso = true;
+            break;
+        }
+    }
+
+    if (testaccesso == true) {
+        alert('Accesso Consentito');
+    } else {
+        alert('Accesso Negato');
+    }
 }
